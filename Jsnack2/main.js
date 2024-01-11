f542deee87997225bd24dbd4117908e3528f6dcc
federico-casu <federico.casu1101@gmail.com>
2024-01-11 14:04:56 +0100

    feat: jsnack2 done

--- Jsnack2/main.js
+++ Jsnack2/main.js
@@ -1,5 +1,19 @@
 /* -------------------------------------------------------------------
 
-
+-Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente.
 
 ------------------------------------------------------------------- */
+
+const n = Number(prompt('Inserisci un numero:'))
+
+document.body.innerHTML = `<ul></ul>`;
+
+const numberList = document.querySelector('ul');
+
+for( i = 1; i <= n; i++ ) {
+    const cube = i * i * i;
+
+    numberList.innerHTML += `<li>${i}^3 = ${cube}</li>`
+
+}
