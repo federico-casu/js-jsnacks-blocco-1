7ca11f4df3b7fd5fabf8b70fcf31d0a2e5e8be7d
federico-casu <federico.casu1101@gmail.com>
2024-01-11 23:36:58 +0100

    feat: jsnack4 done

--- Jsnack3/main.js
+++ Jsnack3/main.js
@@ -1,5 +1,70 @@
 /* -------------------------------------------------------------------
 
-
+Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
 ------------------------------------------------------------------- */
+
+// funzione che genera un numero casuale compreso tra min e max
+function randomNumber(min, max) {
+    return Math.floor((Math.random() * max - min + 1) + min);
+}
+
+function isThere(value, array) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+        if ( value === element ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// lista dei nomi
+const names = [
+    "Giacomo",
+    "Andrea",
+    "Luca",
+    "Carlo",
+    "Enrico"
+]
+
+// lista dei cognomi
+const surnames = [
+    "Rossi",
+    "Bianchi",
+    "Conti",
+    "Del Vecchio",
+    "Vinci"
+]
+
+// lista ospiti
+// let guests = [];
+// let flagNames = [false, false, false, false, false];
+// let flagSurnames = [false, false, false, false, false];
+
+// for (let i = 0; i < names.length; i++) {
+//     let randomName = randomNumber(0, names.length-1);
+//     let randomSurname = randomNumber(0, surnames.length-1);
+    
+//     while (flagNames[randomName] == true) {
+//         randomName = randomNumber(0, names.length-1);
+//     }
+
+//     flagNames[randomName] = true;
+
+//     while (flagSurnames[randomSurname] == true) {
+//         randomSurname = randomNumber(0, surnames.length-1);
+//     }
+
+//     flagSurnames[randomSurname] = true;
+
+//     guests[i] = `${names[randomName]} ${surnames[randomSurname]}`;
+// }
+
+// console.log(guests)
+
+
+
+
--- Jsnack4/main.js
+++ Jsnack4/main.js
@@ -1,5 +1,28 @@
 /* -------------------------------------------------------------------
 
-
+Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
 ------------------------------------------------------------------- */
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let numbers = [];
+let sum = 0;
+
+for (let i = 0; i < 20; i++) {
+    numbers.push(randomNumber(0, 100));
+}
+
+for (let i = 0; i < 20; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+        console.log(numbers[i]);
+        sum += numbers[i];
+    }
+}
+
+console.log(sum);
+
+
