640ed73a9719c5750adfce8ea5b8e84c37d25daf
federico-casu <federico.casu1101@gmail.com>
2024-01-12 00:54:41 +0100

    feat: jsnack3 done

--- Jsnack3/main.js
+++ Jsnack3/main.js
@@ -6,9 +6,10 @@ Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi 
 
 // funzione che genera un numero casuale compreso tra min e max
 function randomNumber(min, max) {
-    return Math.floor((Math.random() * max - min + 1) + min);
+    return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
+// funzione che controlla se un valore è presente nell'array
 function isThere(value, array) {
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
@@ -19,6 +20,30 @@ function isThere(value, array) {
     }
 
     return false;
+}
+
+// funzione ricorsiva che restituisce l'indice della posizione di un nome non ancora stato estratto
+function randomName() {
+    let x = randomNumber(0, names.length-1);
+    
+    if (flagNames[x] === false) {
+        flagNames[x] = true;
+        return x;
+    } else {
+        return randomName();
+    }
+}
+
+// funzione ricorsiva che restituisce l'indice della posizione di un cognome non ancora stato estratto
+function randomSurname() {
+    let x = randomNumber(0, surnames.length-1);
+    
+    if (flagSurnames[x] === false) {
+        flagSurnames[x] = true;
+        return x;
+    } else {
+        return randomSurname();
+    }
 }
 
 // lista dei nomi
@@ -39,32 +64,51 @@ const surnames = [
     "Vinci"
 ]
 
-// lista ospiti
-// let guests = [];
-// let flagNames = [false, false, false, false, false];
-// let flagSurnames = [false, false, false, false, false];
+// lista degli ospiti
+let guests = [];
 
-// for (let i = 0; i < names.length; i++) {
-//     let randomName = randomNumber(0, names.length-1);
-//     let randomSurname = randomNumber(0, surnames.length-1);
-    
-//     while (flagNames[randomName] == true) {
-//         randomName = randomNumber(0, names.length-1);
-//     }
+// array booleani di controllo per il vettore names
+let flagNames = [false, false, false, false, false];
+// array booleani di controllo per il vettore surnames
+let flagSurnames = [false, false, false, false, false];
 
-//     flagNames[randomName] = true;
+// recupero dal DOM gli elementi da manipolare
+const nameListHtml = document.getElementById('names');
+const surnameListHtml = document.getElementById('surnames');
+const guestListHtml = document.getElementById('guests');
 
-//     while (flagSurnames[randomSurname] == true) {
-//         randomSurname = randomNumber(0, surnames.length-1);
-//     }
+// inserisco nella ul con id names gli li contenenti gli elementi dell'array names
+for (let i = 0; i < names.length; i++) {
+    nameListHtml.innerHTML += `<li>${names[i]}</li>`;
+}
 
-//     flagSurnames[randomSurname] = true;
+// inserisco nella ul con id surnames gli li contenenti gli elementi dell'array surnames
+for (let i = 0; i < surnames.length; i++) {
+    surnameListHtml.innerHTML += `<li>${surnames[i]}</li>`;
+}
 
-//     guests[i] = `${names[randomName]} ${surnames[randomSurname]}`;
-// }
+console.log(guests)
 
-// console.log(guests)
+// fintanto che la lunghezza della lista degli ospiti è minore della lunghezza della lista dei nomi (ma poteva essere anche dei cognomi), allora..
+while (guests.length < names.length) {
+    // estraggo l'indice della posizione di un nome che non è ancora stato estratto
+    const x = randomName();
+    // estraggo l'indice della posizione di un cognome che non è ancora stato estratto
+    const y = randomSurname();
 
 
+    console.log(x, y);
+
+    // aggiungo alla lista degli ospiti il nome e il cognome estratti
+    guests.push(`${names[x]} ${surnames[y]}`);
+    console.log(guests.length)
+}
 
 
+ console.log(guests)
+
+
+// inserisco nella ul con id guests gli li contenenti gli elementi dell'array guests
+for (let i = 0; i < guests.length; i++) {
+    guestListHtml.innerHTML += `<li>${guests[i]}</li>`;
+}
